5376c7a6f20d9e90da753fcb5105a3a9399038a0
deksoner1 <brian_94_super@hotmail.com>
2016-06-13 12:07:00 -0500

    Inicio

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="images/robot.ico">
+
+    <title>INTELIGENCIA ARTIFICIAL DISTRIBUIDA</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+     <script src="js/ie-emulation-modes-warning.js"></script>
+
+     <!-- Custom styles for this template -->
+    <link href="css/carousel.css" rel="stylesheet">
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+    
+
+        <nav class="navbar navbar-default navbar-fixed-top navbar-static-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a  href="index.html">  </a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse " >
+              <ul class="nav navbar-nav">
+                <li href="index.html"  ><a href="">Inicio</a></li>
+                <li ><a href="Inteligentes.html">Agentes inteligentes</a></li>
+                <li><a href="Reactivos.html">Agentes Reactivos</a></li>
+                <li><a href="#About">Acerca de...</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                <li><a href="form-humano.html">Iniciar Sesión</a></li>
+               <!-- <li><a href="logout.php" id="Btnlogout">Cerrar Sesión</a></li> -->
+                
+              </ul>
+            </div>
+          </div>
+        </nav></br>
+        
+
+
+    <!-- Carousel
+    ================================================== -->
+   <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="3" class=""></li>
+      </ol>
+      <div class="carousel-inner">         
+        <div class="item active" id="IA1">
+          <img src="images/IA1.jpg" >
+          <div class="container">
+            <div class="carousel-caption">    
+               
+            </div>
+          </div>
+        </div>
+        <div class="item" id="IA2">
+          <img src="images/IA2.png" >
+          <div class="container">
+            <div class="carousel-caption">       
+            </div>
+          </div>
+        </div>
+        <div class="item" id="IA3">
+          <img src="images/IA3.jpg" >
+          <div class="container">
+        
+          </div>
+        </div>
+        <div class="item" id="IA4">
+          <img src="images/IA4.jpg" >         
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+    </div></br></br>
+
+
+  
+
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="#">Regresar arriba</a></p>
+        <p>&copy; 2016 Inteligencia Artificial Distribuida,Agentes Inteligentes. &middot; <a href="#">Todos los derechos reservados.</a> &middot; <a href="#">Contactame , o manda un correo a alopez59@ucol.mx.</a></p>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+    <script src="js/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="js/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+$mysqli = new mysqli('localhost', 'root', '', 'Captcha'); //datos de la bdd
+$usuario = $_POST["user"]; //llaves js
+$contra = $_POST["pass"];
+
+$consult = "SELECT * FROM empleados WHERE user='$usuario' AND pass='$contra'"; //consulta sql
+//$consult = "SELECT * from empleados WHERE user = 'Agustin'";
+
+$vec = array();
+    if($result = $mysqli->query($consult)){
+      while($fila = $result->fetch_assoc()){ $vec[] = $fila; }
+    }  //convertir en array asociativo para poder leer todo lo que se trajo de la bdd
+
+   // echo var_dump($vec);
+    //echo "hola: ".count($vec);
+    //exit;
+    if(count($vec)>0){
+      session_start();
+      $_SESSION['user'] = $usuario;
+      echo json_encode(1); //si por lo menos hay un registro , se regresa un 1 a js
+    }
+    else{
+      echo json_encode(0); //si no encuentra nada, no hay coincidencia y se manda un 0
+    }
+
+
+?>

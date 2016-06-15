67e1943bc5c9481c932e2ce6083dac01d7b75b57
IngGil <Ing.Gil.Cardenas.Alan@gmail.com>
2016-06-15 16:34:12 -0500

    first step

---
+++ find-ip.php
@@ -0,0 +1,21 @@
+<?php
+    function GetUserIP()
+    {
+        if (isset($_SERVER))
+        {
+            if (isset($_SERVER["HTTP_X_FORWARDED_FOR"]))
+                return $_SERVER["HTTP_X_FORWARDED_FOR"];
+            if (isset($_SERVER["HTTP_CLIENT_IP"]))
+                return $_SERVER["HTTP_CLIENT_IP"];
+            return $_SERVER["REMOTE_ADDR"];
+        }
+
+        if (getenv('HTTP_X_FORWARDED_FOR'))
+            return getenv('HTTP_X_FORWARDED_FOR');
+        if (getenv('HTTP_CLIENT_IP'))
+            return getenv('HTTP_CLIENT_IP');
+        return getenv('REMOTE_ADDR');
+    }
+    $a=GetUserIP();
+    echo $a;
+?>
